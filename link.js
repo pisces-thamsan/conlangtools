e51ba3c29ae624f4d6bf93fa72ca14d4dfeaec53
澶山稵杞 <163642689+pisces-thamsan@users.noreply.github.com>
2025-10-04 17:48:54 +0800

    Create link.js

---
+++ link.js
@@ -0,0 +1,10 @@
+// 工具数据
+const tools = [
+    {
+        name: "莱比锡注释生成器",
+        description: "将造语原文、注释和翻译自动排版为莱比锡注释规则的规范格式",
+        category: "composing",
+        icon: "fas fa-spell-check",
+        link: "https://pisces-thamsan.github.io/conlangtools/leipzig/"
+    },
+];
